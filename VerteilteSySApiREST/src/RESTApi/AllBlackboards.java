d2ba56a391d9d311176e0d6d1264e88636498f5e
U51210 <U51210@o7744.rafi.inhouse>
2019-05-29 17:38:24 +0200

    Working GET API - AllBlackboards

--- VerteilteSySApiREST/src/RESTApi/AllBlackboards.java
+++ VerteilteSySApiREST/src/RESTApi/AllBlackboards.java
@@ -1,24 +1,53 @@
 package RESTApi;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Application;
 import org.json.*;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
 import org.json.simple.parser.*;
+
+import com.sun.research.ws.wadl.Response;
 
 
 @Path("/blackboards")
 public class AllBlackboards {
+	
+	
+	
+	 @Path("{sku}")
+	 @PUT
+	 @Consumes(MediaType.APPLICATION_JSON)
+	 public void saveBlackboards(@PathParam("sku") String sku) throws IOException {
+	  
+		 
+		 try {
+			 FileWriter fileWriter = new FileWriter("C:\\Users\\U51210\\git\\verteiltsys\\VerteilteSySApiREST\\model.json");
+			//JSONParser jsonParser = new JSONParser();
+			// jsonParser.parse(sku);
+			 fileWriter.write(sku);
+			 fileWriter.flush(); 
+			 fileWriter.close(); 
+		 }catch (IOException e) {
+			e.printStackTrace();
+		}
+		 
+	    }
+	
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -28,30 +57,23 @@ public class AllBlackboards {
 		
 		//JSON parser object to parse read file
         JSONParser jsonParser = new JSONParser();
-        JSONObject jsonObject = null;
-        String input = null;
+        JSONArray jsonArray = new JSONArray();
          
         //try (FileReader reader = new FileReader("/data/model.json"))
         try (FileReader reader = new FileReader("C:\\Users\\U51210\\git\\verteiltsys\\VerteilteSySApiREST\\model.json"))
         {
-            //Read JSON file
-        	// jsonArray = (JSONArray) jsonParser.parse(reader);
+        	
+        	jsonArray = (JSONArray) jsonParser.parse(reader);
         	
         	
-
-        	 // for (Object o : jsonArray)
-        	 // {
-        	    jsonObject = (JSONObject) jsonParser.parse(reader);
-        	  //}
-            //Iterate over employee array
-            
+        	
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } /*catch (ParseException e) {
+        } catch (ParseException e) {
             e.printStackTrace();
-        }*/
+        }
 		/*
         File f = new File("scores.txt");
     	f.getAbsolutePath();
@@ -65,6 +87,6 @@ public class AllBlackboards {
 		
 		
 		//return jsonObject.toString();//
-        return jsonObject.toString();
+        return jsonArray.toString();
 	}
 }
