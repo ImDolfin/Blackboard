84f5d730dec06239c8b6c7e62ac8d8e127ad54b8
U51210 <U51210@o7744.mshome.net>
2019-05-30 15:43:09 +0200

    GET und PUT funktioniert!

--- VerteilteSySApiREST/src/RESTApi/AllBlackboards.java
+++ VerteilteSySApiREST/src/RESTApi/AllBlackboards.java
@@ -1,27 +1,34 @@
 package RESTApi;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Application;
+
+import org.apache.commons.io.FileUtils;
 import org.json.*;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.*;
 
-import com.sun.research.ws.wadl.Response;
+//import com.sun.research.ws.wadl.Response;
 
 
 @Path("/blackboards")
@@ -29,29 +36,61 @@ public class AllBlackboards {
 	
 	
 	
-	 @Path("{sku}")
+	 @Path("/puttext")
 	 @PUT
 	 @Consumes(MediaType.APPLICATION_JSON)
-	 public void saveBlackboards(@PathParam("sku") String sku) throws IOException {
+	 //@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	 //@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	// public Response saveBlackboards(@QueryParam("text") String text) {
+	 public void saveBlackboards(String text) {
 	  
 		 
 		 try {
 			 FileWriter fileWriter = new FileWriter("C:\\Users\\U51210\\git\\verteiltsys\\VerteilteSySApiREST\\model.json");
 			//JSONParser jsonParser = new JSONParser();
 			// jsonParser.parse(sku);
-			 fileWriter.write(sku);
+			 fileWriter.write(text);
 			 fileWriter.flush(); 
 			 fileWriter.close(); 
 		 }catch (IOException e) {
 			e.printStackTrace();
 		}
 		 
-	    }
+	        //return Response.status(200).build();
+		 
+	}
+	 
+	@Path("/text") 
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getText() throws IOException  
+	{
+		File file = null;
+		String string = null;
+         
+        file = new File("C:\\Users\\U51210\\git\\verteiltsys\\VerteilteSySApiREST\\model.txt");
+        string = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+        
+		/*
+        File f = new File("scores.txt");
+    	f.getAbsolutePath();
+    	input = f.getAbsolutePath();*/
+		
+		/*
+		InputStream input = Converter.class.getResourceAsStream("/data/model.json");
+		String allBlackboards;
+		allBlackboards = input.toString();
+		*/
+		
+		
+		//return jsonObject.toString();//
+        return string;
+	}
 	
-	
+	@Path("/json") 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getBlackboards() throws ParseException
+	public String getBlackboards()
 	{
 		//JSONObject blackboards = new JSONObject();
 		
